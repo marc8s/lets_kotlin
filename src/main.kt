ec476e9bf692fc11c946ed4739d7b0f97899f915
marc8s <marcus.ecomp@gmail.com>
2021-02-18 14:17:27 +0000

    Funções com e sem retorno explicito

--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,14 @@
-fun main(){
-    hello()
-    var name = "Marcus"
-    var age = 29
-    val cpf = "04462511514"
-    println("My name is $name")
+fun calculaIdade(a: Int, b: Int){
+    var idade = a - b
+    println("A idade é $idade")
 }
-
-fun hello(){
-    println("Hello World")
+fun hello(nome: String) : String{
+    return "Olá, $nome"
+}
+fun main(){
+    val anoNascimento = 1992
+    val anoAtual = 2021
+    var nome = "Marcus"
+    calculaIdade(anoAtual, anoNascimento)
+    println(hello(nome))
 }
