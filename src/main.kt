d58ca986ffd6bbf9fbba04a99a3b6918b891afec
marc8s <marcus.ecomp@gmail.com>
2021-02-18 14:32:06 +0000

    Função em uma unica linha

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,17 @@
 fun calculaIdade(a: Int, b: Int){
-    var idade = a - b
-    println("A idade é $idade")
+    println("A idade é ${a-b}")
 }
 fun hello(nome: String) : String{
     return "Olá, $nome"
 }
+
+fun hello2(nome:String) = "Olá, $nome"
+
 fun main(){
     val anoNascimento = 1992
     val anoAtual = 2021
     var nome = "Marcus"
     calculaIdade(anoAtual, anoNascimento)
     println(hello(nome))
+    println(hello2(nome))
 }
